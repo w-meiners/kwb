63d9c226fe31434899e1234ba7ea47f3ce932bd4
w-meiners <w.meiners@reckenberg-berufskolleg.de>
2019-09-18 20:47:05 +0000

    On branch wip modified:   "src/Bel\303\274ftungskonzept.ipynb" new file:   src/Uebung_01.ipynb new file:   src/__pycache__/bel_konzept.cpython-36.pyc modified:   src/bel_konzept.py

--- src/bel_konzept.py
+++ src/bel_konzept.py
@@ -5,4 +5,4 @@ def dV_inf(f_inf=0.5,A_N=None,h=None,n_50=1,delta_p=None):
     if A_N is None or h is None or delta_p is None:
         raise Exception('Parameter nicht angegeben, A_N, h und delta_p müssen spezifiziert werden!')
     
-    return f_inf*A_N*h*n_50*(delta_p/50)^0.667
+    return f_inf*A_N*h*n_50*(delta_p/50)**0.667
