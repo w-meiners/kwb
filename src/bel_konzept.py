57575f090c2dbd2e9f58ba2a98ca1397af71667f
w-meiners <w.meiners@reckenberg-berufskolleg.de>
2019-09-26 17:57:07 +0200

    Changes to be committed: new file:   src/Belueftungskonzept deleted:    "src/Bel\303\274ftungskonzept.ipynb" modified:   src/Uebung_01a.ipynb modified:   src/bel_konzept.py

--- src/bel_konzept.py
+++ src/bel_konzept.py
@@ -1,7 +1,9 @@
 def dV_ges_zu(A_N):
+    """Feststehende Formel nach DIN 1946 Teil 6"""
     return -0.001*A_N**2 + 1.15*A_N + 20
 
 def dV_inf(f_inf=0.5,A_N=None,h=None,n_50=1,delta_p=None):
+    """Feststehende Formel nach DIN 1946 Teil 6"""
     if A_N is None or h is None or delta_p is None:
         raise Exception('Parameter nicht angegeben, A_N, h und delta_p müssen spezifiziert werden!')
     
